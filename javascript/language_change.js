dea609a21d2e0a99df3bca73bdb6fe58e8955418
Krestine H <krha0001@stud.kea.dk>
2025-11-25 15:40:12 +0100

    close model button does not work propper, something wierd when closing

---
+++ javascript/language_change.js
@@ -0,0 +1,13 @@
+const languageChangeBtn = document.querySelector("#btn-user-language");
+const languageModel = document.querySelector("#change-language-model");
+const closeModelBtn = document.querySelector("#close-model");
+
+languageChangeBtn.addEventListener("click", () => {
+    languageModel.showModal();
+    
+});
+
+closeModelBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    languageModel.close();
+});
--- javascript/user_icon.js
+++
@@ -1,6 +0,0 @@
-const dialog = document.querySelector("#user-icon-drop-down");
-const userIcon = document.querySelector("#btn-user-profile");
-
-userIcon.addEventListener("click", () => {
-    dialog.showModal()}
-);
