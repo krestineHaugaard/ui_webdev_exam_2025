286084e71f453a6e9317746bbdf5e4a46cb05d07
Krestine H <krha0001@stud.kea.dk>
2025-11-25 14:53:18 +0100

    had to fix some hiccups

---
+++ javascript/user_icon.js
@@ -0,0 +1,6 @@
+const dialog = document.querySelector("#user-icon-drop-down");
+const userIcon = document.querySelector("#btn-user-profile");
+
+userIcon.addEventListener("click", () => {
+    dialog.showModal()}
+);
