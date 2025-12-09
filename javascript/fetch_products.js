85895a141ebebb98f6df4c5b541009b504ca4990
Krestine H <krha0001@stud.kea.dk>
2025-12-09 17:04:39 +0100

    done a little front page css with pictures and worked on categories and dynamic pages

--- javascript/fetch_products.js
+++ javascript/fetch_products.js
@@ -1,27 +1,52 @@
 
 import { ALL_PRODUCTS_API_URL } from "./fakestoreapi.js";
 
+const params = new URLSearchParams(window.location.search);
+const category = params.get('category');
+
+console.log(category);
+
+if (category === 'men') {
+    document.getElementById('products_title').innerHTML = "Men's Clothing";
+} else if (category === 'women') {
+    document.getElementById('products_title').innerHTML = "Women's Clothing";
+} else {
+    document.getElementById('products_title').innerHTML = "Please pick a category";
+};
+
+const categoryMap = {
+    men: "men's clothing",
+    women: "women's clothing"
+};
+
 const allProducts = () => {
     fetch(`${ALL_PRODUCTS_API_URL}/products`)
     .then(res=>res.json())            
     .then((products) => {
+
+        const filtered = products.filter(product =>
+            product.category === categoryMap[category]
+        );
+
         const productsList = document.querySelector("#products_list");
-        products.forEach((product) => {
+
+        filtered.forEach((product) => {
+            console.log(product)
             productsList.append(productCard(product));
         });
+        
     }).catch(error => console.log(error));
         
 };
 
 const productCard = (product) => {
-    const h3 = document.createElement("h3");
-    h3.innerText = product.title;
-    return h3;
-}
+    const cardTemplate = document.querySelector("#product_card").content.cloneNode(true);
+    cardTemplate.querySelector("img").setAttribute("src", `${product.image}`);
+    cardTemplate.querySelector("img").setAttribute("alt", `${product.title}`);
+    cardTemplate.querySelector("h2").innerText = product.title;
+    cardTemplate.querySelector("h3").innerText = product.price;
+    cardTemplate.querySelector(".product_rating").innerText = product.rating.rate;
+    return cardTemplate;
+};
 
 allProducts();
-
-
-<section id="products_list">
-
-</section>
