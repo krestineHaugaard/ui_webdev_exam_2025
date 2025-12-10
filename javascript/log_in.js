f81f732bd2e6459497dbd919ae26fc84f0f1eeaf
Krestine H <krha0001@stud.kea.dk>
2025-12-10 13:38:52 +0100

    got sign up to work, but it misses some validation

---
+++ javascript/log_in.js
@@ -0,0 +1,3 @@
+// import { USER_BASE_URL } from "./info.js";
+// import { SESSION_STORAGE_USER_EMAIL } from "./info.js";
+
--- javascript/sign_up.js
+++ javascript/sign_up.js
@@ -1,24 +1,20 @@
-import { USER_BASE_URL } from "./info";
+import { USER_BASE_URL } from "./info.js";
 
 document.querySelector("#formSignup").addEventListener("submit", (e) => {
     e.preventDefault();
 
     const userEmail = e.target.txtEmail.value.trim();
-    const password = e.target.txtPassword.value.trim();
-    const repeatPassword = e.target.txtRepeatPassword.value.trim();
+    const userPassword = e.target.txtPassword.value.trim();
+    const userRepeatPassword = e.target.txtRepeatPassword.value.trim();
 
-    console.log(userEmail);
-    console.log(password);
-    console.log(repeatPassword);
-
-    if(password !== repeatPassword){
+    if(userPassword !== userRepeatPassword){
         // make some ui for incorrekt password
         return false;
     }
 
     const newUser = {
-        email: email,
-        password: password
+        email: userEmail,
+        password: userPassword
     }
 
     fetch(`${USER_BASE_URL}/users`, {
@@ -27,6 +23,11 @@ document.querySelector("#formSignup").addEventListener("submit", (e) => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(newUser)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // make some ui for validating sign up
+        e.target.removeEventListener();
     })
     .catch(error => console.log(error));
 });
