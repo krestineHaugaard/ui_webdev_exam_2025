09af60ca90796cca87498babfb461a1ecb386d6b
Krestine H <krha0001@stud.kea.dk>
2025-12-10 14:54:51 +0100

    now you can log in and log out

--- javascript/log_in.js
+++ javascript/log_in.js
@@ -1,3 +1,31 @@
-// import { USER_BASE_URL } from "./info.js";
-// import { SESSION_STORAGE_USER_EMAIL } from "./info.js";
+import { USER_BASE_URL } from "./info.js";
+import { SESSION_STORAGE_USER_EMAIL } from "./info.js";
 
+document.querySelector("#formLogin").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    fetch(`${USER_BASE_URL}/users`)
+    .then(response => response.json())
+    .then(data => {
+        const userEmail = e.target.txtEmail.value.trim();
+        const userPassword = e.target.txtPassword.value.trim();
+
+        let found = false;
+        data.forEach(user => {
+            console.log(user)
+            if (!found) {
+                if (user.email === userEmail && user.password === userPassword) {
+                    sessionStorage.setItem(SESSION_STORAGE_USER_EMAIL, userEmail);
+                    location.href = "index.html";
+
+                    found = true;
+                }
+            }
+        });
+
+        if (!found) {
+            // make some validation for invalid information
+        }
+    })
+    .catch(error => console.log(error));
+});
---
+++ javascript/login_check.js
@@ -0,0 +1,10 @@
+import { SESSION_STORAGE_USER_EMAIL } from "./info.js";
+
+const userEmail = sessionStorage.getItem(SESSION_STORAGE_USER_EMAIL);
+if (userEmail !== null) {
+    document.querySelector("#log-in-btn").classList.add("hidden");
+    document.querySelector("#log-out-btn").classList.remove("hidden");
+} else {
+    document.querySelector("#log-in-btn").classList.remove("hidden");
+    document.querySelector("#log-out-btn").classList.add("hidden");
+}
