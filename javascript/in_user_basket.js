15740583f70039a8cda959655dd9451dc91f30a3
Krestine H <krha0001@stud.kea.dk>
2025-12-17 11:22:58 +0100

    made a UI update so the basket on the home page changes text if your logged in or not

--- javascript/in_user_basket.js
+++ javascript/in_user_basket.js
@@ -7,47 +7,56 @@ if(userEmail !== null){
 
     const userBasket = localStorage.getItem(`${userEmail}`);
 
-    const data = JSON.parse(userBasket);
+    if(userBasket){
 
-    const allProducts = () => {
-        data.products.forEach(product => {
+        const data = JSON.parse(userBasket);
 
-            fetch(`${ALL_PRODUCTS_API_URL}/products`)
-            .then(res=>res.json())            
-            .then((apiProducts) => {
-                
-                const filtered = apiProducts.filter(apiProduct =>
-                    apiProduct.title === product.product_name
-                );
+        const allProducts = () => {
+            data.products.forEach(product => {
 
-                const productList = document.querySelector("#in_user_basket");
+                fetch(`${ALL_PRODUCTS_API_URL}/products`)
+                .then(res=>res.json())            
+                .then((apiProducts) => {
+                    
+                    const filtered = apiProducts.filter(apiProduct =>
+                        apiProduct.title === product.product_name
+                    );
 
-                filtered.forEach((item) => {
-                    console.log(item)
-                    productList.append(productCard(item))
-                });
-                
-            })
-            .catch(error => console.log(error));
+                    const productList = document.querySelector("#in_user_basket");
 
-        });
+                    filtered.forEach((item) => {
+                        console.log(item)
+                        productList.append(productCard(item))
+                    });
+                    
+                })
+                .catch(error => console.log(error));
+
+            });
+        };
+
+        const productCard = (item) => {
+            const cardTemplate = document.querySelector("#product_card").content.cloneNode(true);
+            cardTemplate.querySelector("a").setAttribute("href", `single_product.html?category=${item.category}&title=${item.title}`)
+            cardTemplate.querySelector("img").setAttribute("src", `${item.image}`);
+            cardTemplate.querySelector("img").setAttribute("alt", `${item.title}`);
+            cardTemplate.querySelector("h4").innerText = item.title;
+            cardTemplate.querySelector(".product-prize").innerText = item.price;
+            cardTemplate.querySelector(".product_rating").innerText = item.rating.rate;
+            return cardTemplate;
+        };
+
+        allProducts();
+
+    }else{
+        const productList = document.querySelector("#in_user_basket");
+        const p = document.createElement("p");
+        p.innerText = "You don't have any items in your basket... yet ;)"
+        productList.append(p);
     };
-
-    const productCard = (item) => {
-        const cardTemplate = document.querySelector("#product_card").content.cloneNode(true);
-        cardTemplate.querySelector("a").setAttribute("href", `single_product.html?category=${item.category}&title=${item.title}`)
-        cardTemplate.querySelector("img").setAttribute("src", `${item.image}`);
-        cardTemplate.querySelector("img").setAttribute("alt", `${item.title}`);
-        cardTemplate.querySelector("h4").innerText = item.title;
-        cardTemplate.querySelector(".product-prize").innerText = item.price;
-        cardTemplate.querySelector(".product_rating").innerText = item.rating.rate;
-        return cardTemplate;
-    };
-
-    allProducts();
 }else{
     const productList = document.querySelector("#in_user_basket");
     const p = document.createElement("p");
-    p.innerText = "You don't have any items in your basket... yet ;)"
+    p.innerText = "You need to log in to see your basket"
     productList.append(p);
 }
